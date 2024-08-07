8332f25239e8d9cc5c91748f9d9cd695934feea5
icaro <ic4ro.p4blo@gmail.com>
2024-08-07 20:55:20 -0300

    openbsd build

--- config.def.h
+++ config.def.h
@@ -97,24 +97,50 @@ unsigned int tabspaces = 4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+	/* "#282828", /1* hard contrast: #1d2021 / soft contrast: #32302f *1/ */
+    "#1d2021",
+	"#cc241d",
+	"#98971a",
+	"#d79921",
+	"#458588",
+	"#b16286",
+	"#689d6a",
+	"#a89984",
+	"#928374",
+	"#fb4934",
+	"#b8bb26",
+	"#fabd2f",
+	"#83a598",
+	"#d3869b",
+	"#8ec07c",
+	"#ebdbb2",
+	[255] = 0,
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#add8e6", /* 256 -> cursor */
+	"#555555", /* 257 -> rev cursor*/
+	"#282828", /* 258 -> bg */
+	"#ebdbb2", /* 259 -> fg */
+/* }; */
 
-	"#000000",
-	"#eb3c3e",
-	"#986b4e",
-	"#e7584e",
-	"#fc7062",
-	"#d2a66b",
-	"#27718c",
-	"#91c7cc",
+/* static const char *colorname[] = { */
 
-	"#648a8e",
-	"#eb3c3e",
-	"#986b4e",
-	"#e7584e",
-	"#fc7062",
-	"#d2a66b",
-	"#27718c",
-	"#91c7cc",
+/* 	"#000000", */
+/* 	"#eb3c3e", */
+/* 	"#986b4e", */
+/* 	"#e7584e", */
+/* 	"#fc7062", */
+/* 	"#d2a66b", */
+/* 	"#27718c", */
+/* 	"#91c7cc", */
+
+/* 	"#648a8e", */
+/* 	"#eb3c3e", */
+/* 	"#986b4e", */
+/* 	"#e7584e", */
+/* 	"#fc7062", */
+/* 	"#d2a66b", */
+/* 	"#27718c", */
+/* 	"#91c7cc", */
 
 
 /*	
@@ -138,11 +164,11 @@ static const char *colorname[] = {
 	"cyan",
 	"white",
 */
-	[255] = 0,
+	/* [255] = 0, */
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	/* "#cccccc", */
+	/* "#555555", */
 };
 
 
@@ -150,9 +176,11 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
+/* unsigned int defaultfg = 7; */
+unsigned int defaultfg = 15;
 unsigned int defaultbg = 0;
-unsigned int defaultcs = 256;
+/* unsigned int defaultcs = 256; */
+unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
